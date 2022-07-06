0ccf6a0da46ad12f5bc70e0af48a393f46e0fac5
sunnytarawade <sunnytarawade000@gmail.com>
2022-07-06 19:52:10 +0530

    Turned value type of all filter arguments to list

--- faang_gsoc/graphql_api/grapheneObjects/protocol_analysis/arguments/filter.py
+++ faang_gsoc/graphql_api/grapheneObjects/protocol_analysis/arguments/filter.py
@@ -1,17 +1,17 @@
 from graphene import InputObjectType, String, Field,List
 
 class Analyses_InputField(InputObjectType):
-    accession = String()
-    organism = String()
-    datasetAccession = String()
-    analysisType = String()
+    accession = List(String)
+    organism = List(String)
+    datasetAccession = List(String)
+    analysisType = List(String)
 
 class ProtocolAnalysisFilterBasic_Argument(InputObjectType):
-    universityName = String()
-    protocolDate = String()
-    protocolName = String()
-    key = String()
-    url = String()
+    universityName = List(String)
+    protocolDate = List(String)
+    protocolName = List(String)
+    key = List(String)
+    url = List(String)
     analyses = Field(Analyses_InputField)
 
 class ProtocolAnalysisFilterJoin_Argument(InputObjectType):
